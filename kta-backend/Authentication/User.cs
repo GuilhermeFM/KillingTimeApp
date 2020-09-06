e0a3b547216c3007bdd53765f3436375815f850c
Guilherme F. Meira <guifrizzera2005@gmail.com>
2020-09-06 19:35:41 -0300

    Removing trash from project

---
+++ kta-backend/Authentication/User.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace kta.Authentication
+{
+    public class User : IdentityUser
+    {
+    }
+}
---
+++ kta-backend/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using kta.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace kta.Controllers
+{
+    [ApiController]
+    public class HomeController : Controller
+    {
+        [Authorize]
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return Ok(new Response { Status = "Success", Message = "Welcome to home" });
+        }
+
+        [Route("AboutMe")]
+        public IActionResult AboutMe()
+        {
+            return Ok(new Response { Status = "Success", Message = "My Infos" });
+        }
+    }
+}
