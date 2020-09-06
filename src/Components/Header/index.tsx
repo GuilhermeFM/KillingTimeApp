d1b95ba97f5e16fbf3f58152b78d5874ebdee8b3
Guilherme F. Meira <guifrizzera2005@gmail.com>
2020-09-05 21:54:20 -0300

    Added Format script to package.json

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Container } from './styles';
 
 const Header: React.FC = () => {
-  return <Container></Container>;
+  return <Container />;
 };
 
 export default Header;
