9426e55e82a5116fd89df62e99a0234d7a5dab8f
Guilherme F Meira <guifrizzera2005@gmail.com>
2020-10-15 15:36:15 -0300

    Added kta-queue with hangfire

---
+++ kta-backend/kta-queue/Startup.cs
@@ -0,0 +1,64 @@
+using Hangfire;
+using Hangfire.SqlServer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace kta_queue
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        public Startup(IWebHostEnvironment webHostEnvironment, IConfiguration configuration)
+        {
+            WebHostEnvironment = webHostEnvironment;
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var hangfireConnectionString = Configuration.GetConnectionString("ConnStr");
+            var sqlServerStorageOptions = new SqlServerStorageOptions
+            {
+                CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+                SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+                QueuePollInterval = TimeSpan.Zero,
+                UseRecommendedIsolationLevel = true,
+                DisableGlobalLocks = true,
+            };
+
+            services.AddHangfire(configuration =>
+            {
+                configuration
+                    .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+                    .UseSimpleAssemblyNameTypeSerializer()
+                    .UseRecommendedSerializerSettings()
+                    .UseSqlServerStorage(hangfireConnectionString, sqlServerStorageOptions);
+            });
+
+            services.AddHangfireServer();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseHangfireDashboard();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHangfireDashboard();
+            });
+        }
+    }
+}
